731e970aaff8b0ab4258cd1b6c1cc2415dd2075d
diegomated <61214524+diegomated@users.noreply.github.com>
2020-03-26 21:23:27 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,6 @@
+num_a = int(input("numero a: "))
+num_c = int(input("numero c: "))
+
+print("Suma de a + c: ",num_a+num_c)
+print("multiplicacion de a + c: ",num_a*num_c)
+print("el doble de a: ",num_a*2)
