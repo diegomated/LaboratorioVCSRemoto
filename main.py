49ecbbae2aecf99e43ac9c3c62081236b8dd5a1c
diegomated <61214524+diegomated@users.noreply.github.com>
2020-03-26 21:40:16 -0500

    Hallar las x de una funcion cuadratica UwU

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import math
-num_a = int(input("numero a: "))
-num_b = int(input("numero b: "))
-num_c = int(input("numero c: "))
-num_d = int(input("numero d: "))
+a = int(input("numero a: "))
+b = int(input("numero b: "))
+c = int(input("numero c: "))
 
-print("Suma de a + c: ",num_a+num_c)
-print("multiplicacion de a + c: ",num_a*num_c)
-print("el doble de a: ",num_a*2)
-print("cuadrado de b: ",num_b**2)
-print("raiz cuadrada de d: ",math.sqrt(num_d))
+d=b**2-4*a*c
+
+if(d>0):
+    x1=(-b+math.sqrt(d))/2*a
+    x2=(-b-math.sqrt(d))/2*a
+    print("x1= ",x1)
+    print("x2= ",x2)
+elif(d==0):
+    print("x1 es igual a x2")
+    x1=-b/2*a
+    print("x= ",x1)
+else:
+    print("no existe una solucion para esta ecuacion, dentro de los R")
+
