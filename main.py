08347a4f54cdc09e20f89878fab7b6489ead2855
diegomated <61214524+diegomated@users.noreply.github.com>
2020-03-26 21:31:07 -0500

    Mi primera modificacion UwU

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import math
 num_a = int(input("numero a: "))
+num_b = int(input("numero b: "))
 num_c = int(input("numero c: "))
+num_d = int(input("numero d: "))
 
 print("Suma de a + c: ",num_a+num_c)
 print("multiplicacion de a + c: ",num_a*num_c)
 print("el doble de a: ",num_a*2)
+print("cuadrado de b: ",num_b**2)
+print("raiz cuadrada de d: ",math.sqrt(num_d))
