b6fcf55d53d185a59e7974692eee9e234e34d2fa
diegomated <61214524+diegomated@users.noreply.github.com>
2020-04-14 09:41:57 -0500

    punto 1

---
+++ preinforme10_DiegoCardenas.py
@@ -0,0 +1,7 @@
+import numpy as np 
+kellogs=np.array([27834,23789,30189,30967,32501,32701,31665,17155,4614,834])
+p1=(kellogs[0]+kellogs[1])/2
+p2=(kellogs[9]+kellogs[8])/2
+
+print("Diferencia de los 2 primeros años a los 2 ultimos años: ", p1-p2)
+
