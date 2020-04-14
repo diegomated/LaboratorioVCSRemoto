82294c89c8728df3c83957ddc9b17ad535e83c34
diegomated <61214524+diegomated@users.noreply.github.com>
2020-04-14 10:00:56 -0500

    punto 2

--- preinforme10_DiegoCardenas.py
+++ preinforme10_DiegoCardenas.py
@@ -5,3 +5,17 @@ p2=(kellogs[9]+kellogs[8])/2
 
 print("Diferencia de los 2 primeros años a los 2 ultimos años: ", p1-p2)
 
+mayor=0
+menor=0
+for i in range (0,10,1):
+    if i==0 :
+        mayor=kellogs[0]
+    elif kellogs[i]>mayor:
+        mayor=kellogs[i]
+
+    if i==0 :
+        menor=kellogs[0]
+    elif kellogs[i]<menor:
+        menor=kellogs[i]
+
+print("Diferencia de la mayor y menor utilidad: ", mayor-menor)
