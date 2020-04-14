f55578055a4b16e56a284aa98e8cd44543086901
diegomated <61214524+diegomated@users.noreply.github.com>
2020-04-14 12:20:35 -0500

    punto 3, php (arreglo punto 1)

--- preinforme10L_DiegoCardenas.php
+++ preinforme10L_DiegoCardenas.php
@@ -1,11 +1,11 @@
 <?php
 
 $kellogs=array(27834,23789,30189,30967,32501,32701,31665,17155,4614,834);
-$acu=0;
-for($i=0;$i<10;$i++){
-    $acu=$acu+$kellogs[$i];
-}
-echo "El promedio es: ".$acu."<br/>";
+$p1=($kellogs[0]+$kellogs[1])/2;
+$p2=($kellogs[count($kellogs)-1]+$kellogs[count($kellogs)-2])/2;
+$dif=$p1-$p2;
+echo "Diferencia de los 2 primeros años a los 2 ultimos años: ".$dif."<br/>";
+
 
 $menor=0;
 $mayor=0;
@@ -22,6 +22,24 @@ for($i=0;$i<10;$i++){
     }
 }
 $dif=$mayor-$menor;
-echo "la diferencia de la mayor y menor utilidad es: ".$dif;
+
+echo "La diferencia de la mayor y menor utilidad es: ".$dif."<br/>";
+
+$acu1=0;
+$kellogs2=array(27834,23789,30189,30967,32501,32701,31665,17155,4614,834);
+for($i=0;$i<10;$i++){
+    for($a=0;$a<9;$a++){
+        if($kellogs2[$a]>$kellogs2[$a+1]){
+            $acu1=$kellogs2[$a];
+            $kellogs2[$a]=$kellogs2[$a+1];
+            $kellogs2[$a+1]=$acu1;
+        }
+    }
+}
+
+$m1=count($kellogs2)/2;
+$m2=(count($kellogs2)/2)+1;
+$mediana=($kellogs2[$m1-1]+$kellogs2[$m2-1])/2;
+echo "La mediana es: ".$mediana;
 
 ?>
